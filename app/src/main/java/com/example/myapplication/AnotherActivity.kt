bb83201e95dea53ba500573708b124970aa64359
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-19 00:50:31 +0300

    wasn't that easy but done

--- app/src/main/java/com/example/myapplication/AnotherActivity.kt
+++ app/src/main/java/com/example/myapplication/AnotherActivity.kt
@@ -2,11 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
-class HomeActivity : AppCompatActivity() {
+class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_another)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = intent.getStringExtra("data")
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,28 +3,40 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.LayoutInflater
+import android.view.View.VISIBLE
+import android.view.View.inflate
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        var intent = Intent(this, MainActivity::class.java) // im forced on this haha
-        when (item.itemId){
-            R.id.home_item -> intent = Intent(this, HomeActivity::class.java)
-            R.id.about_item -> intent = Intent(this, AboutActivity::class.java)
-            R.id.help_item -> intent = Intent(this, HelpActivity::class.java)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            alert()
         }
-        startActivity(intent)
-        return super.onOptionsItemSelected(item)
+    }
+
+    private fun alert(){
+        val dialogBuilder = AlertDialog.Builder(this)
+        val dialogLayout = layoutInflater.inflate(R.layout.alert_layout, null)
+        val editText = dialogLayout.findViewById<EditText>(R.id.alert_et)
+        val button = dialogLayout.findViewById<Button>(R.id.alert_btn)
+        val textView = dialogLayout.findViewById<TextView>(R.id.alert_tv)
+        dialogBuilder.setView(dialogLayout)
+        button.setOnClickListener { textView.text = editText.text }
+        dialogBuilder.setPositiveButton("Go") { _, _ ->
+            startActivity(Intent(this, AnotherActivity::class.java).putExtra("data", editText.text.toString()))
+        }
+        val alert = dialogBuilder.create()
+        alert.show()
     }
 }
