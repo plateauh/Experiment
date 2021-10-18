0af4aeced164048c18d203169039f7e1301aa468
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-18 23:37:49 +0300

    done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,19 +10,40 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button: Button
-    lateinit var editText: EditText
+    lateinit var buttons: List<Button>
+    lateinit var editTexts: List<EditText>
     lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editText = findViewById(R.id.some_et)
+
+        editTexts = listOf (
+                findViewById(R.id.some_et1),
+                findViewById(R.id.some_et2),
+                findViewById(R.id.some_et3))
+
+        buttons = listOf (
+                findViewById(R.id.toast_btn),
+                findViewById(R.id.tv_btn),
+                findViewById(R.id.activity_btn))
+
         textView = findViewById(R.id.some_tv)
-        button = findViewById(R.id.btn)
-        button.setOnClickListener {
-            textView.text = editText.text
+
+        buttons[0].setOnClickListener {
+            Toast.makeText(this, editTexts[0].text, Toast.LENGTH_SHORT).show()
         }
+
+        buttons[1].setOnClickListener {
+            textView.text = editTexts[1].text
+        }
+
+        buttons[2].setOnClickListener {
+            val intent = Intent(this, AnotherActivity::class.java)
+            intent.putExtra("data", editTexts[2].text.toString())
+            startActivity(intent)
+        }
+
     }
 
 }
