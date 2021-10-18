43f91084d3ab3350892d1ee406b4e87dbe1210e3
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-18 23:15:27 +0300

    2 btns

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,107 +11,21 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btn: Button
-    lateinit var username: EditText
-    lateinit var password: EditText
-    lateinit var rePassword: EditText
-    lateinit var message: TextView
+    lateinit var kotlinButton: Button
+    lateinit var xmlButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn = findViewById(R.id.signupbtn)
-        username = findViewById(R.id.username)
-        password = findViewById(R.id.tePassword)
-        rePassword = findViewById(R.id.teRePassword)
-        message = findViewById(R.id.tvmsg)
-
-        btn.setOnClickListener {
-            if (usernameChecked() && passwordChecked() && rePasswordChecked())
-                message.text = "You are good to go!"
+        kotlinButton = findViewById(R.id.ktln_btn)
+        kotlinButton.setOnClickListener {
+            Toast.makeText(this, "Coding", Toast.LENGTH_SHORT).show()
         }
+        xmlButton = findViewById(R.id.xml_btn)
     }
 
-    private fun passwordChecked(): Boolean {
-
-        return when {
-            password.text.isEmpty() -> {
-                message.text = "Please enter a password."
-                false
-            }
-            password.text.length < 8 -> {
-                message.text = "The password is too short, please enter 8 or more characters."
-                false
-            }
-            else -> true
-        }
-
-    }
-
-    private fun rePasswordChecked(): Boolean {
-
-        return when {
-            rePassword.text.isEmpty() -> {
-                message.text = "Please re-enter the password."
-                false
-            }
-            rePassword.text.toString() != password.text.toString() -> {
-                message.text = "The password & password re-entered don't match."
-                Log.d("Password matching", "Password: ${password.text} | Repassword: ${rePassword.text} | Matching: ${password.text.toString() == rePassword.text.toString()}")
-                false
-            }
-            else -> true
-        }
-
-    }
-
-    private fun usernameChecked(): Boolean {
-
-        return when {
-            username.text.isEmpty() -> {
-                message.text = "Please enter a username."
-                false
-            }
-            username.text.contains(" ") -> {
-                message.text = "No spaces are allowed in username."
-                false
-            }
-            username.text.length > 10 -> {
-                message.text = "Please enter shorter username, 10 characters max."
-                false
-            }
-            username.text[0].isDigit() -> {
-                message.text = "Username mustn't starts with a digit"
-                false
-            }
-            else -> true
-        }
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Toast.makeText(this, "My lovely app is onStart()", Toast.LENGTH_LONG).show()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Toast.makeText(this, "My lovely app is onPause()", Toast.LENGTH_LONG).show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Toast.makeText(this, "My lovely app is onResume()", Toast.LENGTH_LONG).show()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Toast.makeText(this, "My lovely app is onStop()", Toast.LENGTH_LONG).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(this, "My lovely app is onDestroy()", Toast.LENGTH_LONG).show()
+    fun xmlButtonListener(view: View){
+        Toast.makeText(this, "XML", Toast.LENGTH_SHORT).show()
     }
 }
